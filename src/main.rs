5bcda2616daf51b1b8ae95c922a076a2f97b82f2
Saad <saadmunawarpc@gmail.com>
2026-09-19 21:00:42 +0500

    Pushing to github

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         string_question = getting_string.trim().to_string();
 
     } else {
-        string_question = String::from("[saad[ahmad{zimal}saad]ahmad]fatima");
+        string_question = String::from("");
     }
 
     println!("");
